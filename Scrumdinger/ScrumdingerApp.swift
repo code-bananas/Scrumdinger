ed0c002755a6a6e68824eb66f6a153aa50dd3e7b
Andrew Lester <andrewjlester@hotmail.com>
2022-10-05 21:25:47 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Andrew Lester on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
